42fa72b5874eef4010ba5610153c3c6b2da560c5
unknown <saidbouferriche@gmail.com>
2023-01-11 17:43:09 +0100

    adding tests

--- microservices/recommendation-service/src/main/java/com/example/recommendationservice/RecommendationServiceApplication.java
+++ microservices/recommendation-service/src/main/java/com/example/recommendationservice/RecommendationServiceApplication.java
@@ -2,8 +2,10 @@ package com.example.recommendationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.example")
 public class RecommendationServiceApplication {
 
 	public static void main(String[] args) {
